1146d2fd117900b70373a558e97727b77fe3fb3c
irfanAzmi1999 <eesyrafeerfan90@gmail.com>
2023-05-03 16:42:00 +0800

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,9 +37,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
         });
 
-        // resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
 
-        // parent::boot();
+        parent::boot();
     }
 
     /**
