d9d821e1e941243bb57591905854a5bf0a96195c
irfanAzmi1999 <eesyrafeerfan90@gmail.com>
2023-05-05 10:53:31 +0800

    fix shadow

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,10 +36,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
-
-        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
-
-        parent::boot();
+//
+//        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+//
+//        parent::boot();
     }
 
     /**
