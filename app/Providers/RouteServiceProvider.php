7d4a35fb3a785af5062a2387f38fc9cf9e459d38
irfanAzmi1999 <eesyrafeerfan90@gmail.com>
2023-08-07 08:50:09 +0800

    SSL FIX

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,8 +36,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
-//           resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
-//           parent::boot();
+           resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+           parent::boot();
     }
 
     /**
